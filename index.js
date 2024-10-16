5c57a8ed7d5f6733a385645b92da7eb993703e35
LNeuman91 <lneuman819@gmail.com>
2024-10-16 09:39:50 -0700

    Updated index.js with new froyo functionality

--- index.js
+++ index.js
@@ -7,33 +7,49 @@
 //Is your logic organized into a function? Done
 
 const froyoFunction = () => {
-  let userInput = prompt("Whats your favorite froyo flavor?");
+  let userInput = prompt("What's your favorite froyo flavor?");
   console.log(userInput);
 
   const froyoFlavors = userInput.split(" ");
+  console.log(`Number of flavors entered: ${froyoFlavors.length}`);
 
+  const flavorObj = {}; // Object to track observed flavors
 
-  console.log(froyoFlavors.length);
-
-  const flavorObj = {};
-
+  // Loop through the input flavors
   for (let i = 0; i < froyoFlavors.length; i++) {
-    console.log(froyoFlavors[i]);
+    let flavor = froyoFlavors[i];
+
+    // Update the object with the observed flavor count
+    if (flavorObj[flavor]) {
+      flavorObj[flavor]++;
+    } else {
+      flavorObj[flavor] = 1;
+    }
   }
-}
 
+  console.log("Observed flavors:", flavorObj);
 
-// for (let i = froyoFlavors.length -1; i >= 0; i--) {
-//   console.log(froyoFlavors[i])
-// }
-
+  // for (let i = froyoFlavors.length -1; i >= 0; i--) {
+  //   console.log(froyoFlavors[i])
+  // }
 
   const froyo = {
-  vanilla: 12,
-  chocolate: 20,
-  strawberry: 8,
-  rockyRoad: 14,
-  caramel: 16,
+    vanilla: 12,
+    chocolate: 20,
+    strawberry: 8,
+    rockyRoad: 14,
+    caramel: 16,
+  };
+
+  for (let flavor of froyoFlavors) {
+    if (froyo[flavor]) {
+      console.log(
+        `${flavor} is available with ${froyo[flavor]} servings left.`
+      );
+    } else {
+      console.log(`${flavor} is not available.`);
+    }
+  }
 };
 
-console.log();
+froyoFunction();
